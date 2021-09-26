cb71bd84e5cb492905c1e45e6cd153e490698fdb
Elmor-PC <Elmor-PC@DESKTOP-NG3QI99>
2021-09-26 13:01:51 +0800

    Add project files.

---
+++ SideNavigation/wwwroot/js/sidenav.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(() => {
+	// SideNav Button Initialization
+	$(".button-collapse").sideNav2();
+	$(".button-collapse").click();
+	// SideNav Scrollbar Initialization
+	var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+	var ps = new PerfectScrollbar(sideNavScrollbar);
+});
